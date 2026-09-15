b751d4ca1dd98d448e4c17b1c40665227255641e
Vishnu A S <asvishnukaruvannur7@gmail.com>
2026-09-16 00:43:33 +0530

    hi

--- src/test/java/com/orange/utils/ExcelUtil.java
+++ src/test/java/com/orange/utils/ExcelUtil.java
@@ -1,30 +1,33 @@
 package com.orange.utils;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtil {
 	public static Object[][] getTestData(String path,String sheetname) throws IOException{
-		FileInputStream fis = new FileInputStream(path);
-		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		XSSFSheet sheet = wb.getSheet(sheetname);
-		
-		int rows = sheet.getPhysicalNumberOfRows();
-		int cols = sheet.getRow(0).getPhysicalNumberOfCells();
-		
-		Object[][] data = new Object[rows-1][cols];
-		
-		for(int i=1;i<rows;i++) {
-			for(int j=0;j<cols;j++) {
-				data[i-1][j]=sheet.getRow(i).getCell(j).toString();
-			}
+		InputStream input = ExcelUtil.class.getClassLoader().getResourceAsStream(path);
+		if (input == null) {
+			throw new FileNotFoundException(path + " not found in test resources");
 		}
-		wb.close();
-		return data;
-		
-		
+
+		try (XSSFWorkbook wb = new XSSFWorkbook(input)) {
+			XSSFSheet sheet = wb.getSheet(sheetname);
+
+			int rows = sheet.getPhysicalNumberOfRows();
+			int cols = sheet.getRow(0).getPhysicalNumberOfCells();
+
+			Object[][] data = new Object[rows - 1][cols];
+
+			for (int i = 1; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					data[i - 1][j] = sheet.getRow(i).getCell(j).toString();
+				}
+			}
+			return data;
+		}
 	}
 }
